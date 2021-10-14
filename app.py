6d273a6ce274a22ec1943d37a2b5a9e4a8ed9b53
KenObie <kobrien@hey.com>
2021-10-13 19:10:30 -0700

    removed passwords lol

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 host = "localhost"
 user = "root"
-passwd = "kohojo97"
+passwd = ""
 database = "cse6242"
 geo_ids_file = "data/GeoIDCounty.csv"
 geo_id_table = "geo_ids"
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'kohojo97'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'cse6242'
 
 
